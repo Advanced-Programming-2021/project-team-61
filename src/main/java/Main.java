bdd9a4ec4b13790318a261b17a8ec2edcfdc6a40
@ARB1380 <azerila.farshi@gmail.com>
2021-04-27 20:36:12 +0430

    database of cards added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,20 @@
-public class Main{
+import java.io.File;
+
+
+public class Main {
     public static void main(String[] args) {
+
+        File f = new File("src\\main\\resources\\Monster.csv");
+        String absolute = f.getAbsolutePath();
+        File f2 = new File("src\\main\\resources\\SpellTrap.csv");
+        String absolute2 = f2.getAbsolutePath();
+        Database.readDataLineByLine(absolute, absolute2);
+
         RegisterMenu r = RegisterMenu.getInstance();
         r.run();
-        //test them
+
+
     }
-
-
-
-
-
-
 
 
 }
