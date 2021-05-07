0a99d24a994e288dcfa3ae2ca9c7744fd48fe292
am_afl <aflatoonian.1380@gmail.com>
2021-05-08 02:44:40 +0430

    first code for SelectClass(need changes)

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -359,5 +359,14 @@ public class Board {
     public String[] getSpellTrapZone() {
         return spellTrapZone;
     }
+
+    public HashMap<Integer, Card> getMonsterCardsInField() {
+        return monsterCardsInField;
+    }
+
+    public HashMap<Integer, Card> getSpellTrapCardsInField() {
+        return spellTrapCardsInField;
+    }
+
     ////////////////
 }
