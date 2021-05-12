f8427270e238bafe8b53708f4cdc0d995b8950f2
@ARB1380 <azerila.farshi@gmail.com>
2021-05-13 00:52:59 +0430

    improved controller view well

--- src/main/java/RegisterMenu.java
+++ src/main/java/RegisterMenu.java
@@ -1,9 +1,7 @@
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class RegisterMenu {
     private static RegisterMenu r = null;
-    private String command;
     private RegisterView view;
 
 
@@ -19,69 +17,30 @@ public class RegisterMenu {
         return r;
     }
 
-    public void run() {
-        while (true) {
-            int changed = 0;
-            command = view.scan();
-            if (command.startsWith("user create ")) {
-                changed = 1;
-                String regex = "user create --username ([^\\s]+) --nickname ([^\\s]+) --password ([^\\s]+)";
-                Matcher matcher = getCommandMatcher(command, regex);
-                if (matcher.find()) {
-                    if (Player.isUserNameExists(matcher.group(1)))
-                        view.printMessage(RegisterView.Commands.USEREXISTSWITHUSERNAME, matcher.group(1));
-                    else if (Player.isNickNameExists(matcher.group(2)))
-                        view.printMessage(RegisterView.Commands.USEREXISTSWITHNICKNAME, matcher.group(2));
-                    else {
-                        CreatePlayer(matcher.group(1), matcher.group(2), matcher.group(3));
-                    }
-                }
+    public void userCreateProcess(Matcher matcher) {
+        if (Player.isUserNameExists(matcher.group(1)))
+            view.printMessage(RegisterView.Commands.userExistsWithUsername, matcher.group(1));
+        else if (Player.isNickNameExists(matcher.group(2)))
+            view.printMessage(RegisterView.Commands.userExistsWithNickname, matcher.group(2));
+        else
+            CreatePlayer(matcher.group(1), matcher.group(2), matcher.group(3));
+    }
 
+    public void loginProcess(Matcher matcher) {
+        if (!Player.isUserNameExists(matcher.group(1)))
+            view.printMessage(RegisterView.Commands.noMatch, "");
+        else if (!Player.getPlayerByUsername(matcher.group(1)).getPassword().equals(matcher.group(2)))
+            view.printMessage(RegisterView.Commands.noMatch, "");
+        else {
+            view.printMessage(RegisterView.Commands.LOGIN, "");
+            MainMenu m = MainMenu.getInstance();
+            m.run(matcher.group(1));
 
-            }
-            if(command.startsWith("user login")){
-                changed = 1;
-                String regex = "user login --username ([^\\s]+) --password ([^\\s]+)";
-                Matcher matcher = getCommandMatcher(command,regex);
-                if(matcher.find()){
-                    if(!Player.isUserNameExists(matcher.group(1)))
-                        view.printMessage(RegisterView.Commands.NOMATCH,"");
-                    else if(!Player.getPlayerByUsername(matcher.group(1)).getPassword().equals(matcher.group(2)))
-                        view.printMessage(RegisterView.Commands.NOMATCH,"");
-                    else{
-                        view.printMessage(RegisterView.Commands.LOGIN,"");
-                        MainMenu m = MainMenu.getInstance();
-                        m.run(matcher.group(1));
-
-                    }
-                }
-            }
-            if(command.equals("menu show-current")){
-                changed = 1;
-                view.printMessage(RegisterView.Commands.SHOWMENU,"");
-            }
-            if(command.startsWith("menu enter")){
-                changed = 1;
-                view.printMessage(RegisterView.Commands.FIRSTLOGIN,"");
-            }
-            if (command.equals("menu exit") && changed == 0) {
-                break;
-            }
-            if (changed == 0)
-                view.printMessage(RegisterView.Commands.INVALID, "");
         }
-
-
     }
 
-    private void CreatePlayer(String username, String nickname, String password) {
+    private void CreatePlayer(String username, String nickname, String password){
         new Player(username, nickname, password);
-        view.printMessage(RegisterView.Commands.SUCCESSFULL, "");
-    }
-
-    private Matcher getCommandMatcher(String input, String regex) {
-        Pattern p = Pattern.compile(regex);
-        return p.matcher(input);
-
+        view.printMessage(RegisterView.Commands.successful, "");
     }
 }
