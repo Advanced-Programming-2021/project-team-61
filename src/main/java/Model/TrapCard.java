5f044f727ff1c84143b49720a43fed43c1a90d26
am_afl <aflatoonian.1380@gmail.com>
2021-05-15 01:51:34 +0430

    printACardComplete in TrapCard

--- src/main/java/Model/TrapCard.java
+++ src/main/java/Model/TrapCard.java
@@ -10,4 +10,11 @@ public class TrapCard extends Card{
         allCards.add(this);
     }
 
+    public void printACardComplete(TrapCard card){
+        System.out.println("Name : " + card.cardName);
+        System.out.println("Trap");
+        System.out.println("Type : " + card.status);
+        System.out.println("Description : " + card.description);
+    }
+
 }
