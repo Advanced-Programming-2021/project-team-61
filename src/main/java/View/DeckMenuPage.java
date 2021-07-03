ea65a5db490e093b295ce21c0fa19d376d55e598
am-afl <aflatoonian.1380@gmail.com>
2021-07-04 00:02:53 +0430

    first code for DeckMenuPage.java

--- src/main/java/View/DeckMenuPage.java
+++ src/main/java/View/DeckMenuPage.java
@@ -1,4 +1,63 @@
 package View;
 
-public class DeckMenuPage {
+import Model.Player;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import Controller.DeckMenu;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DeckMenuPage implements Initializable {
+
+    @FXML
+    private TextField deckName;
+
+    @FXML
+    private Button removeDeck;
+
+    @FXML
+    private Button createDeck;
+
+    @FXML
+    private Button activate;
+
+    @FXML
+    private Button edit;
+
+    @FXML
+    private Button back;
+
+    public void createDeck(){
+        DeckMenu.getInstance().createDeck(deckName.getText(), Player.getLoggedPlayer());
+    }
+
+    public void removeDeck(){
+        DeckMenu.getInstance().deleteDeck(deckName.getText(), Player.getLoggedPlayer());
+    }
+
+    public void edit(){
+        //go to a page that show deck information...
+        //that can add or remove a card to/from deck
+    }
+
+    public void activate(){
+        DeckMenu.getInstance().setActivateDeck(deckName.getText(), Player.getLoggedPlayer());
+    }
+
+    public void back() throws IOException {
+        Logic.viewManager.changeScene("/sample/mainMenuPage.fxml");
+    }
+
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
 }
