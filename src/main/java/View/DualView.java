bfae2b22f10e2fd018e14fcfe6671448deb8d988
am-afl <aflatoonian.1380@gmail.com>
2021-07-04 17:21:06 +0430

    graphical changes in DualView.java

--- src/main/java/View/DualView.java
+++ src/main/java/View/DualView.java
@@ -1,11 +1,12 @@
 package View;
 
+import javax.swing.*;
+
 public class DualView {
     public enum Commands {
         playerTwoNotExist,
         hasNoActiveDeck,
         roundInvalid,
-        inValid,
         deckInvalid
     }
 
@@ -25,23 +26,18 @@ public class DualView {
     public void printMessage(Commands message, String s) {
         switch (message) {
             case playerTwoNotExist: {
-                System.out.println("there is no player with this username");
+                JOptionPane.showConfirmDialog(null,"there is no player with this username","Message",JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+                //System.out.println("there is no player with this username");
                 break;
             }
             case hasNoActiveDeck: {
-                System.out.println(s + " has no active deck");
-                break;
-            }
-            case roundInvalid: {
-                System.out.println("number of rounds is not supported");
-                break;
-            }
-            case inValid: {
-                System.out.println("invalid command");
+                JOptionPane.showConfirmDialog(null,s + " has no active deck","Message",JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+                //System.out.println(s + " has no active deck");
                 break;
             }
             case deckInvalid:{
-                System.out.println(s+ " 's deck is invalid");
+                JOptionPane.showConfirmDialog(null,s+ " 's deck is invalid","Message",JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+                //System.out.println(s+ " 's deck is invalid");
                 break;
             }
         }
