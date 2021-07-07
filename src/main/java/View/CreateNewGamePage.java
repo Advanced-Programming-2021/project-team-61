b9428e34d092b554c9af793d3c7107355543fe86
ARB1380 <azerila.farshi@gmail.com>
2021-07-07 21:32:37 +0430

    add photo of field

--- src/main/java/View/CreateNewGamePage.java
+++ src/main/java/View/CreateNewGamePage.java
@@ -2,12 +2,23 @@ package View;
 
 import Model.Player;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import Controller.DualMenu;
 
-public class CreateNewGamePage {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateNewGamePage implements Initializable {
+    private Image[] images = new Image[14];
+    private static int i = 0;
+    @FXML
+    private ImageView fieldImages;
 
     @FXML
     private Text round;
@@ -41,5 +52,47 @@ public class CreateNewGamePage {
 
     public void start(){
         DualMenu.getInstance().ProcessNewGame(Player.getLoggedPlayer().getUsername(),username.getText(),numOfRound.getText());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        images[0] = new Image("resources/Images/fie_burn.bmp");
+        images[1] = new Image("resources/Images/fie_dark.bmp");
+        images[2] = new Image("resources/Images/fie_desert.bmp");
+        images[3] = new Image("resources/Images/fie_fusion.bmp");
+        images[4] = new Image("resources/Images/fie_gaia.bmp");
+        images[5] = new Image("resources/Images/fie_mori.bmp");
+        images[6] = new Image("resources/Images/fie_normal.bmp");
+        images[7] = new Image("resources/Images/fie_sanctu.bmp");
+        images[8] = new Image("resources/Images/fie_shine.bmp");
+        images[9] = new Image("resources/Images/fie_sougen.bmp");
+        images[10] = new Image("resources/Images/fie_umi.bmp");
+        images[11] = new Image("resources/Images/fie_water.bmp");
+        images[12] = new Image("resources/Images/fie_yama.bmp");
+        images[13] = new Image("resources/Images/fie_yami.bmp");
+    }
+
+    public void showNextFieldGame(MouseEvent mouseEvent) {
+        i = i + 1;
+        if(i > 13){
+            i = 14;
+            fieldImages.setImage(null);
+        }
+        else{
+            fieldImages.setImage(images[i]);
+        }
+
+
+    }
+
+    public void showPreviousFieldGame(MouseEvent mouseEvent) {
+        i = i - 1;
+        if(i < 0){
+            i = -1;
+            fieldImages.setImage(null);
+        }
+        else{
+            fieldImages.setImage(images[i]);
+        }
     }
 }
