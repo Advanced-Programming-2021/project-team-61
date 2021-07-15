b31f386845f58640f19190301e35d5d10b82fe41
ARB1380 <azerila.farshi@gmail.com>
2021-07-15 16:07:41 +0430

    abc

--- Server/src/main/java/Main.java
+++ Server/src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("mmd");
+        System.out.println("ali");
     }
 }
