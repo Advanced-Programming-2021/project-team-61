8d5ad18f8e29b2df5cc91f8ac021d2b66b6dd87d
am-afl <aflatoonian.1380@gmail.com>
2021-07-21 14:25:00 +0430

    add shop changes

---
+++ Client/src/main/java/Model/SpellCard.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class SpellCard extends Card{
+
+    private String status;
+
+    public SpellCard(String cardName,String cardType,String description,String status,int price){
+        super(cardName,cardType,description,price);
+        this.status = status;
+        allCards.add(this);
+
+    }
+
+    public static void printACardComplete(SpellCard card){
+        System.out.println("Name : " + card.cardName);
+        System.out.println("Spell");
+        System.out.println("Type : " + card.status);
+        System.out.println("Description : " + card.description);
+    }
+}
